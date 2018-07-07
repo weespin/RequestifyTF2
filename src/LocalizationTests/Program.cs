6e279c87f3e0019229e82c771e2ccf8d8631349c
William <wnmerukini@gmail.com>
2018-07-07 22:30:46 +0300

    Fixed Ukrainian. Still need some checks

---
+++ src/LocalizationTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalizationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode; 
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN");
+       
+            Console.WriteLine(Localization.Localization.TF_CHAT_TEAM);
+            Console.Read();
+        }
+    }
+}
