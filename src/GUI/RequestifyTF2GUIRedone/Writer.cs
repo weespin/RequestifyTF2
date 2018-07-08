82fa285aba0a4f4227e88373894694b6d08f4355
William <wnmerukini@gmail.com>
2018-07-08 21:46:54 +0300

    Fixed

--- src/GUI/RequestifyTF2GUIRedone/Writer.cs
+++ src/GUI/RequestifyTF2GUIRedone/Writer.cs
@@ -9,33 +9,22 @@ namespace RequestifyTF2GUIRedone
     {
         public TextBlock _outp2;
         public TextBox _output;
-
         public TextBoxStreamWriter(TextBox output, TextBlock outp2)
         {
             _output = output;
             _outp2 = outp2;
         }
-
         public override Encoding Encoding => Encoding.UTF8;
 
         public override void Write(char value)
         {
             base.Write(value);
-
             _output.Dispatcher.BeginInvoke(new Action(delegate { _output.AppendText(value.ToString()); }));
-
-
-            Write2(value);
-        }
-
-
-        public void Write2(char val)
-        {
             _outp2.Dispatcher.BeginInvoke(new Action(delegate
             {
                 if (_outp2.Text.EndsWith(Environment.NewLine)) _outp2.Text = null;
 
-                _outp2.Text += val.ToString();
+                _outp2.Text += value.ToString();
             }));
         }
     }
