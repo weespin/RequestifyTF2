d5501286f28d6c777b65ef697cd734accac7b83d
William <wnmerukini@gmail.com>
2018-08-14 04:29:19 +0300

    Added Youtube Link placeholder

--- src/GUI/RequestifyTF2GUIRedone/Controls/BindsTab.xaml.cs
+++ src/GUI/RequestifyTF2GUIRedone/Controls/BindsTab.xaml.cs
@@ -158,7 +158,61 @@ namespace RequestifyTF2GUIRedone.Controls
                     cell.Link = file;
                 }
             }
+
+            if (cell.BindType == "YoutubeMusic")
+            {
+              
+                ExecuteRunDialog(sender);
+            }
         }
-      
+        private async void ExecuteRunDialog(object o)
+        {
+            //let's set up a little MVVM, cos that's what the cool kids are doing:
+            var view = new SampleDialog
+            {
+                DataContext = new SampleDialogViewModel()
+            };
+
+            //show the dialog
+            var result = await DialogHost.Show(view, "RootDialog", ClosingEventHandler);
+
+            //check the result...
+            Console.WriteLine("Dialog was closed, the CommandParameter used to close it was: " + (result ?? "NULL"));
+        }
+        private void ClosingEventHandler(object sender, DialogClosingEventArgs eventArgs)
+        {
+            Console.WriteLine("You can intercept the closing event, and cancel here.");
+        }
+
+
+    }
+    public class SampleDialogViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                this.MutateVerbose(ref _name, value, RaisePropertyChanged());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged()
+        {
+            return args => PropertyChanged?.Invoke(this, args);
+        }
+    }
+    public static class NotifyPropertyChangedExtension
+    {
+        public static void MutateVerbose<TField>(this INotifyPropertyChanged instance, ref TField field, TField newValue, Action<PropertyChangedEventArgs> raise, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TField>.Default.Equals(field, newValue)) return;
+            field = newValue;
+            raise?.Invoke(new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ src/GUI/RequestifyTF2GUIRedone/Controls/YoutubeDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace RequestifyTF2GUIRedone.Controls
+{
+    /// <summary>
+    /// Interaction logic for SampleDialog.xaml
+    /// </summary>
+    public partial class SampleDialog : UserControl
+    {
+        public SampleDialog()
+        {
+            InitializeComponent();
+        }
+    }
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                ? new ValidationResult(false, "Field is required.")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
